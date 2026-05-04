fd6145dabd99f36fc98f7c1f6ee2ed974b93c801
ilasenko220237-ux <ivanov220237@gmail.com>
2026-05-04 09:45:10 +0700

    Update test_app.py

--- 454 код будущего/test_app.py
+++ 454 код будущего/test_app.py
@@ -6,7 +6,6 @@ from datetime import datetime
 class TestTrainingPlanner(unittest.TestCase):
     
     def test_date_validation_correct(self):
-        """Позитивный тест: корректная дата."""
         date_str = "2026-05-04"
         try:
             datetime.strptime(date_str, "%Y-%m-%d")
@@ -16,8 +15,7 @@ class TestTrainingPlanner(unittest.TestCase):
         self.assertTrue(is_valid)
 
     def test_date_validation_incorrect(self):
-        """Негативный тест: неверный формат даты."""
-        date_str = "04-05-2026" # Не тот формат
+        date_str = "04-05-2026"
         try:
             datetime.strptime(date_str, "%Y-%m-%d")
             is_valid = True
@@ -26,7 +24,6 @@ class TestTrainingPlanner(unittest.TestCase):
         self.assertFalse(is_valid)
 
     def test_duration_validation_positive(self):
-        """Позитивный тест: положительная длительность."""
         duration = "45"
         try:
             val = int(duration)
@@ -37,7 +34,6 @@ class TestTrainingPlanner(unittest.TestCase):
         self.assertTrue(is_valid)
 
     def test_duration_validation_negative(self):
-        """Негативный тест: отрицательная длительность."""
         duration = "-10"
         try:
             val = int(duration)
@@ -48,7 +44,6 @@ class TestTrainingPlanner(unittest.TestCase):
         self.assertFalse(is_valid)
 
     def test_json_save_load(self):
-        """Тест сохранения и загрузки JSON."""
         test_data = [{"id": 1, "date": "2026-05-04", "type": "Бег", "duration": 30}]
         filename = "test_temp.json"
         
